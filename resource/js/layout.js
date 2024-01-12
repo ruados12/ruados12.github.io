9b5e6e8d0166cb2278933912dafece8f09196994
ruados12 <ruados12@gmail.com>
2024-01-12 16:53:05 +0900

    초기

---
+++ resource/js/layout.js
@@ -0,0 +1,125 @@
+// const rand = (min, max) => {
+//     ~~ 연산자는 소수점 이하를 버리고 정수로 반환
+//     return ~~(Math.random() * (max - min + 1)) + min;
+// }
+
+
+class Layout {
+    constructor(){
+        this.header = document.querySelector('#header');
+        this.toggle = this.header.querySelector('.toggle_btn');
+        this.allMenu = document.querySelector('#allMenu');
+    }
+    toggleEvt() {
+        this.toggle.addEventListener('click', evt => {
+            this.toggle.classList.toggle('on');
+            if(this.toggle.classList.contains('on')) {
+                this.openAllMenu();
+            }else {
+                this.closeAllMenu();
+            }
+        })
+    }
+    openAllMenu() {
+        this.allMenu.classList.add('on');
+        bubble.activeBubble();
+    }
+    closeAllMenu() {
+        this.allMenu.classList.remove('on');
+        bubble.cancelBubble();
+    }
+}
+
+class Bubble {
+    constructor() {
+        this.cvs = document.querySelector('#bubble');
+        this.ctx = this.cvs.getContext('2d');
+        this.bubble = [];
+        this.square = {};
+        this.w = window.innerWidth;
+        this.h = window.innerHeight;
+        this.square = {
+            w: this.w,
+            h: this.h,
+            y: this.h,
+            speed: 0.5,
+            totalTime: 250,
+            duration: 250,
+            time: 0,
+            delay: 15,
+        }
+        this.cvs.width = this.w
+        this.cvs.height = this.h;
+        this.bubbleCount = 200;
+        // this.color = `rgba(255,176,158,.75)`;
+        this.color = `rgba(255,176,158,1)`;
+        this.anim;
+        this.ctx.clearRect(0, 0, this.w, this.h);
+        for(let i = 0; i < this.bubbleCount; i++) {
+            this.bubble.push(this.getBubble());
+        }
+    }
+    activeBubble() {
+        if(this.anim) cancelAnimationFrame(this.anim);
+        const render = () => {
+            this.ctx.clearRect(this.w * -0.5, this.h * -0.5, this.w * 1.5, this.h * 1.5);
+            this.ctx.fillStyle = this.color;
+            if(Number(this.square.y) > 0) {
+                // 가속도
+                let sq = this.square;
+                sq.totalTime -+ sq.time;
+                // ease-out
+                sq.y = sq.y + ((sq.time - sq.delay) / sq.duration) * sq.speed * (0 - sq.y);
+                this.square.time += 1;
+            }else {
+                this.square.y = 0;
+                this.totalTime = this.duration;
+                cancelAnimationFrame(this.anim);
+            }
+            this.ctx.beginPath();
+            this.ctx.rect(0, this.square.y, this.square.w, this.square.h);
+            this.ctx.fill();
+            for(const bb of this.bubble) {
+                this.ctx.beginPath();
+                this.ctx.arc(bb.x, bb.y, bb.r, 0, Math.PI * 2);
+                this.ctx.fillStyle = this.color;
+                this.ctx.fill();
+    
+                bb.y = bb.y - (bb.speed * 1);
+                if(bb.y <= bb.r * -2) {
+                    bb.y = rand(this.h, this.h * 2);
+                    bb.speed = rand(1, 25);
+                }
+            }
+            this.anim = requestAnimationFrame(render);
+        }
+        this.anim = requestAnimationFrame(render);
+    }
+    cancelBubble() {
+        if(this.anim){
+            cancelAnimationFrame(this.anim)
+            this.ctx.clearRect(0, 0, this.w, this.h);
+            this.bubble = [];
+            this.square.y = this.h;
+            this.square.time = 0;
+            this.totalTime = this.duration;
+            for(let i = 0; i < this.bubbleCount; i++) {
+                this.bubble.push(this.getBubble());
+            }
+        };
+    }
+    
+    getBubble() {
+        const b = {};
+        b.x = rand(0, w);
+        b.y = rand(this.h, this.h * 2.5);
+        b.r = rand(50, 175);
+        b.speed = rand(5, 35);
+        // b.speed = 100;
+        return b;
+    }
+}
+
+const layout = new Layout();
+const bubble = new Bubble();
+layout.toggleEvt();
---
+++ resource/js/main.js
@@ -0,0 +1,193 @@
+var cvs = document.getElementById("cvs");
+var ctx = cvs.getContext("2d");
+var w = window.innerWidth;
+var h = window.innerHeight;
+var dpr = window.devicePixelRatio;
+cvs.width = w * dpr;
+cvs.height = h * dpr;
+ctx.scale(dpr, dpr);
+ctx.clearRect(0, 0, w, h);
+// ctx.fillStyle = '#efefef';
+ctx.fillRect(0, 0, w, h);
+ctx.fill();
+
+var dots = [];
+var dotsCount = 300;
+
+const rand = function (min, max) {
+    return ~~(Math.random() * (max - min + 1)) + min;
+};
+
+function Hue() {
+    this.h = rand(0, 250);
+}
+Hue.prototype = {
+    update: function () {
+        this.h = (this.h += 0.25) % 360;
+    },
+    getHue: function () {
+        return this.h;
+    },
+};
+
+function Dotted() {}
+Dotted.prototype = {
+    mx: 0,
+    my: 0,
+    rise: 0,
+    run: 0,
+    radian: 0,
+    radius: 2,
+    degree: 0,
+    setPos: function (x, y) {
+        this.x = x;
+        this.y = y;
+        this.dx = rand(0, w);
+        this.dy = rand(0, h);
+    },
+    update: function () {
+        if (this.dx == Math.round(this.x) && this.dy == Math.round(this.y)) {
+            this.dx = rand(0, w);
+            this.dy = rand(0, h);
+        }
+        if (this.x >= w || this.x < 0 || this.y >= h || this.y < 0) {
+            this.dx = rand(0, w);
+            this.dy = rand(0, h);
+        }
+        this.radian = Math.atan2(this.dy - this.y, this.dx - this.x);
+        this.x += Math.cos(this.radian) * 0.5;
+        this.y += Math.sin(this.radian) * 0.5;
+    },
+    draw: function () {
+        var dt = this;
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(255,255,255,1)";
+        ctx.arc(dt.x, dt.y, dt.radius, 0, Math.PI * 180);
+        ctx.fill();
+    },
+};
+for (var i = 0; i < dotsCount; i++) {
+    var d = new Dotted();
+    d.setPos(rand(0, w), rand(0, h));
+    dots.push(d);
+}
+
+var flagMouse = false;
+var mousePos = {};
+cvs.addEventListener("mouseenter", function () {
+    flagMouse = true;
+});
+cvs.addEventListener("mousemove", function () {
+    if (flagMouse) {
+        mousePos.x = event.pageX;
+        mousePos.y = event.pageY;
+    }
+});
+cvs.addEventListener("mouseleave", function () {
+    flagMouse = false;
+});
+
+function Range() {
+    this.x = 0;
+    this.y = 0;
+    this.dx = 0;
+    this.dy = 0;
+    this.bx = 0;
+    this.by = 0;
+    this.mx = 0;
+    this.my = 0;
+    this.radian = 0;
+    this.run = 0;
+    this.rise = 0;
+    this.alpha = 1;
+    this.rangeRadius = 125;
+    this.dtg;
+    this.btg;
+}
+var particles = [];
+Range.prototype = {
+    x: 0,
+    y: 0,
+    update: function () {
+        for (var i = 0; i < dots.length; i++) {
+            dtg = dots[i];
+            for (var j = i + 1; j < dots.length; j++) {
+                btg = dots[j];
+                this.bx = dtg.x - btg.x;
+                this.by = dtg.y - btg.y;
+                if (
+                    Math.sqrt(this.bx * this.bx + this.by * this.by) /
+                        this.rangeRadius <=
+                    1
+                ) {
+                    particles.push({
+                        moveX: dtg.x,
+                        moveY: dtg.y,
+                        toX: btg.x,
+                        toY: btg.y,
+                        alpha:
+                            1 -
+                            Math.sqrt(this.bx * this.bx + this.by * this.by) /
+                                this.rangeRadius,
+                    });
+                    // particles.push({moveX: dtg.x, moveY: dtg.y, toX: btg.x, toY: btg.y, alpha: 1});
+                }
+            }
+            if (flagMouse) {
+                this.mx = dtg.x - mousePos.x;
+                this.my = dtg.y - mousePos.y;
+                if (
+                    Math.sqrt(this.mx * this.mx + this.my * this.my) /
+                        this.rangeRadius <=
+                    1
+                ) {
+                    // particles.push({moveX: dtg.x, moveY: dtg.y, toX: mousePos.x, toY: mousePos.y, alpha: (1 - Math.sqrt((this.mx * this.mx) + (this.my * this.my)) / this.rangeRadius)});
+                    particles.push({
+                        moveX: dtg.x,
+                        moveY: dtg.y,
+                        toX: mousePos.x,
+                        toY: mousePos.y,
+                        alpha:
+                            1 -
+                            Math.sqrt(this.mx * this.mx + this.my * this.my) /
+                                this.rangeRadius,
+                    });
+                }
+            }
+        }
+    },
+};
+
+var hue = new Hue();
+var r = new Range();
+var bt = 0;
+var loop = function (time) {
+    hue.update();
+
+    // ctx.fillStyle = '#efefef';
+    // ctx.fillRect(0, 0, w, h);
+    ctx.clearRect(0, 0, w, h);
+    ctx.fill();
+    particles = [];
+    r.update();
+    for (var i = 0; i < dots.length; i++) {
+        var dt = dots[i];
+        dt.update();
+        dt.draw();
+    }
+    for (var i = 0; i < particles.length; i++) {
+        var pp = particles[i];
+        ctx.beginPath();
+        ctx.moveTo(pp.moveX, pp.moveY);
+        ctx.lineTo(pp.toX, pp.toY);
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        // ctx.strokeStyle = `hsla(${hue.getHue()},46%,54%, ${pp.alpha})`;
+        ctx.strokeStyle = `rgba(255,255,255, ${pp.alpha})`;
+        ctx.stroke();
+    }
+
+    requestAnimationFrame(loop);
+};
+
+requestAnimationFrame(loop);
